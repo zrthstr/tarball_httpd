0becd79f712df5904a5afb4bb2971e411f65d6f2
zrthstr <zrth1k@gmail.com>
2018-12-14 09:45:26 +0100

    clean up sublime tab/space mess

--- proto.py
+++ proto.py
@@ -7,30 +7,31 @@ from urllib.parse import urlparse
 
 class TarHTTP(SimpleHTTPRequestHandler):
 
+    def do_GET(self):
+        print("in do_GET")
+        print(dir(self))
+        print("XXX",self.requestline)
+        args = urlparse(self.requestline).query.split(" ")[0]
+        if "dl=tar" in args:
+            print("Found 'dl=tar' in args")
+            ffff = open("/tmp/out")
+            self.copyfile(ffff, self.wfile)
+        else:
+            print("nope")
+            self.really_do_GET()
 
-	def do_GET(self):
-		print("in do_GET")
-		print(dir(self))
-		print("XXX",self.requestline)
-		args = urlparse(self.requestline).query.split(" ")[0]
-		if "dl=tar" in args:
-			print("Found 'dl=tar' in args")
-			ffff = open("/tmp/out")
-			self.copyfile(ffff, self.wfile)
-		else:
-			print("nope")
-			self.really_do_GET()
+        print(self.headers)
 
 
-	def really_do_GET(self):
-		print("in really_do_GET")
-		f = self.send_head()
-		if f:
-			try:
-				# def copyfile(self, source, outputfile):
-				self.copyfile(f, self.wfile)
-			finally:
-				f.close()
+    def really_do_GET(self):
+        print("in really_do_GET")
+        f = self.send_head()
+        if f:
+            try:
+                # def copyfile(self, source, outputfile):
+                self.copyfile(f, self.wfile)
+            finally:
+                f.close()
 
 
 httpd = HTTPServer(('localhost', 8000), TarHTTP)
