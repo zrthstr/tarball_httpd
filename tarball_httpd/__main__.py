ce9755220ed85083b066e21697ed9e4f32979cd9
zrthstr <zrth1k@gmail.com.com>
2019-03-31 19:13:14 +0200

    Bump version: 0.0.1 → 0.0.2

--- tarball_httpd/__main__.py
+++ tarball_httpd/__main__.py
@@ -34,7 +34,7 @@ from functools import partial
 from http.server import SimpleHTTPRequestHandler, test
 from http import HTTPStatus
 
-__version__ = '0.0.1'
+0.0.2
 
 
 class TarHTTPServer(SimpleHTTPRequestHandler):
